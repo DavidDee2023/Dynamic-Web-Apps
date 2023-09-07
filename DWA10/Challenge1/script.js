4d4f4c1797824e930837c7342b90e7bb89569986
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-09-07 10:39:02 +0200

    Modified the Challenge1 in DWA10 and created a README.md file with details

--- DWA10/Challenge1/script.js
+++ DWA10/Challenge1/script.js
@@ -1,50 +1,64 @@
+// scripts.js
+
 const MAX_NUMBER = 10;
 const MIN_NUMBER = -10;
 const STEP_AMOUNT = 1;
 const RESET_VALUE = 0;
 
 const domElements = {
-    number : document.querySelector('[data-key="number"]'),
-    subtract : document.querySelector('[data-key="subtract"]'),
-    add : document.querySelector('[data-key="add"]'),
-    reset : {
-        reset : document.querySelector('[data-key="reset"]'),
-        resetOverlay : document.querySelector('[data-key="reset-button"]')
+    number: document.querySelector('[data-key="number"]'),
+    subtract: document.querySelector('[data-key="subtract"]'),
+    add: document.querySelector('[data-key="add"]'),
+    reset: {
+        reset: document.querySelector('[data-key="reset"]'),
+        resetOverlay: document.querySelector('[data-key="reset-button"]')
     }
 }
 
-const subtractHandler = () =>{
+const subtractHandler = () => {
     const newValue = parseInt(domElements.number.value) - STEP_AMOUNT;
     domElements.number.value = newValue;
 
-    if(domElements.add.disabled === true){
+    if (domElements.add.disabled === true) {
         domElements.add.disabled = false;
     }
 
-    if(newValue <= MIN_NUMBER){
+    if (newValue <= MIN_NUMBER) {
         domElements.subtract.disabled = true;
     }
 }
 
-const addHandler = () =>{
+const addHandler = () => {
     const newValue = parseInt(domElements.number.value) + STEP_AMOUNT;
     domElements.number.value = newValue;
 
-    if(domElements.subtract.disabled === true){
+    if (domElements.subtract.disabled === true) {
         domElements.subtract.disabled = false;
     }
-    
-    if(newValue >= MAX_NUMBER){
+
+    if (newValue >= MAX_NUMBER) {
         domElements.add.disabled = true;
     }
 }
 
-const resetHandler = () => {    
+
+//he resetHandler function resets the number input field to RESET_VALUE.
+//It also checks if domElements.reset.resetOverlay exists and has a show() method (to avoid errors), and if so, it calls the show() method.
+
+const resetHandler = () => {
     domElements.number.value = RESET_VALUE;
 
-    domElements.reset.resetOverlay.show();    
+    // Using if statement to check if resetOverlay has a show() method
+    if (domElements.reset.resetOverlay && typeof domElements.reset.resetOverlay.show === 'function') {
+        domElements.reset.resetOverlay.show();
+    }
 }
 
 domElements.add.addEventListener('click', addHandler);
 domElements.subtract.addEventListener('click', subtractHandler);
 domElements.reset.reset.addEventListener('click', resetHandler);
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Dynamic-Web-Apps
+# DYNAMIC-WEB-APPLICATIONS
+# TALLY APP
+
+
+Tally App Number Manipulation JavaScrip
+
+This Folder/Project contains a JavaScript code that demonstrates how to manipulate a numeric value within specified bounds using HTML and JavaScript. The code controls a numeric input field and three buttons: "Add," "Subtract," and "Reset." The primary functionality includes:
+
+
+Addition/Increment by 1: Clicking the "Add" button increments the numeric value by a predefined step amount until it reaches a maximum value, disabling the button at that point.
+
+Subtraction/Decreement: Clicking the "Subtract" button decrements the numeric value by the same step amount until it reaches a minimum value, disabling the button accordingly.
+
+Resetting: Clicking the "Reset" button resets the numeric value to a predefined default value and, if applicable, interacts with an element that has a show() method.
