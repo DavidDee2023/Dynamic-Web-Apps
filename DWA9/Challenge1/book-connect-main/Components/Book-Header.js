b64ab4346f3f87979fc06c1bcaea6a737113a005
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2023-09-07 09:19:49 +0200

    made changes to DWA9

--- DWA9/Challenge1/book-connect-main/Components/Book-Header.js
+++ DWA9/Challenge1/book-connect-main/Components/Book-Header.js
@@ -2,18 +2,15 @@ class BookHeader extends HTMLElement {
     constructor() {
       super();
   
-      // Create a shadow DOM for the component
       const shadowRoot = this.attachShadow({ mode: 'open' });
   
-      // Define the HTML content of the component
       this.shadowRoot.innerHTML = `
         <header class="header">
-          <!-- Your header content here -->
-        </header>
+          
       `;
     }
   }
   
-  // Define the custom element for the BookHeader component
+  // I will define the custom element for the BookHeader component
   customElements.define('book-header', BookHeader);
   
